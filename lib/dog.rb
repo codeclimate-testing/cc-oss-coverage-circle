e4245e8888a275569a4ec2e3c8351fb02abee671
Max Jacobson <max@hardscrabble.net>
2017-02-17 13:07:36 -0500

    add repo with specs and coverage generated

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  def initialize(name)
+    @name = name
+  end
+
+  # This method is currently covered by a test
+  def name
+    "#{@name}!!!"
+  end
+
+  def fafa(baz = baz)
+    puts baz
+  end
+
+  # This method is NOT currently covered by a test
+  def age_in_dog_years
+    405
+  end
+
+  def bad(baba = baba)
+    puts baba
+  end
+end
---
+++ spec/dog_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+require "dog"
+
+RSpec.describe Dog do
+  describe "#name" do
+    it "is super excited about its name" do
+      expect(Dog.new("Milo").name).to eq("Milo!!!")
+    end
+
+    it "is super excited about its age" do
+      expect(Dog.new("Milo").age_in_dog_years).to eq(405)
+    end
+  end
+end
